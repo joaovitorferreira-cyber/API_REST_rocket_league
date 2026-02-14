957590debacfa9c1aeb7c4077d3591de75b1f148
joao <joaovitorferreira@paucasesnovescifp.cat>
2026-02-14 22:32:58 +0100

    Definición de la clase Carroceria

---
+++ models/carroceria.js
@@ -0,0 +1,11 @@
+class Carroceria {
+    constructor(id, nombre, calidad, hitbox, lanzamiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.calidad = calidad;
+        this.hitbox = hitbox;
+        this.lanzamiento = lanzamiento;
+    }
+}
+
+module.exports = Carroceria
